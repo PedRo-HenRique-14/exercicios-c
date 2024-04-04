4a91ea41fdfa0890f494283461de8bb3945dbd18
Dodas_ <ph005009004@gmail.com>
2024-04-04 16:08:20 -0300

    Atividade 8 refeita

--- atividades-complementares-dois/Atividade8.c
+++ atividades-complementares-dois/Atividade8.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(void) {
-	int n1, n2, n3;
+	int n1, n2, n3, x = 0;
 	
 	printf("Digite o primeiro numero: ");
 	scanf("%i", &n1);
@@ -15,5 +15,23 @@ int main(void) {
 	printf("Digite o terceiro numero: ");
 	scanf("%i", &n3);
 	fflush(stdin);
+	
+	if (n1 > n2) {
+        x = n1;
+        n1 = n2;
+        n2 = x;
+    }
+    if (n2 > n3) {
+        x = n2;
+        n2 = n3;
+        n3 = x;
+    }
+    if (n1 > n2) {
+        x = n1;
+        n1 = n2;
+        n2 = x;
+    }
+
+    printf("Ordem: %i, %i, %i.", n1, n2, n3);
 	return 0;
 }
