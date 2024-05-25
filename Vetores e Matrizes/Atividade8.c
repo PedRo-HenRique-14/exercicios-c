613a5fbb45b2cb0f85d3d1626003927937b1452b
Dodas_ <53160578+PedRo-HenRique-14@users.noreply.github.com>
2024-05-24 21:37:42 -0300

    Adiciona atividades 7 e 8

---
+++ Vetores e Matrizes/Atividade8.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAMANHO 3
+int main(void) {
+	int mat[TAMANHO][TAMANHO], x, l, c;
+	
+	for(l = 0; l < TAMANHO; l++) { 
+		for(c = 0; c < TAMANHO; c++) {
+			printf("Digite um numero [%d][%d]\n>> ", l+1, c+1);
+			scanf("%d", &mat[l][c]);
+			fflush(stdin);
+		}
+	}
+	
+	printf("Digite o valor de x\n>> ");
+	scanf("%d", &x);
+	fflush(stdin);
+	
+	int i;
+	for(i = 0; i < TAMANHO; i++) {
+		mat[i][i] *= x;
+	}
+	
+	for(l = 0; l < TAMANHO; l++) { 
+		for(c = 0; c < TAMANHO; c++) {
+			printf("%d ", mat[l][c]);
+			if(c == TAMANHO - 1){
+				printf("\n");
+			}
+		}
+	}
+	
+	return 0;
+}
